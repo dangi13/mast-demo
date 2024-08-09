238b94cb702be4d55318f48c0d51d2b584653a97
dangi13 <40757536+dangi13@users.noreply.github.com>
2024-08-09 19:21:40 +0530

    Add files via upload

--- RestAssuredAPITest.java
+++ RestAssuredAPITest.java
@@ -2,6 +2,7 @@ package mast.api.test.mast;
 
 import static io.restassured.RestAssured.given;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import io.restassured.RestAssured;
@@ -10,27 +11,23 @@ import io.restassured.specification.RequestSpecification;
 import mast.api.utils.common.Config;
 
 public class RestAssuredAPITest {
- 
-	
+
 	public static void main(String[] args) {
-		RestAssured.baseURI = "https://gist.githubusercontent.com/kumarpani/1e759f27ae302be92ad51ec09955e765/raw/184cef7125e6ef5a774e60de31479bb9b2884cb5/TeamRCB.json"; 
-		RequestSpecification requestSpecification = RestAssured.given(); 
-		Map<String, String> authParams = Map.of("grant_type", Config.getProperty("GRANT_TYPE"), 
-				"audience", Config.getProperty("AUDIENCE"), 
-                "clientSecret", Config.getProperty("CLIENT_SECRET"),
-                "client_id", Config.getProperty("CLIENT_ID")); 
+		RestAssured.baseURI = "https://gist.githubusercontent.com/kumarpani/1e759f27ae302be92ad51ec09955e765/raw/184cef7125e6ef5a774e60de31479bb9b2884cb5/TeamRCB.json";
+		RequestSpecification requestSpecification = RestAssured.given();
+
+		Map<String, String> authParams = new HashMap<String, String>();
+		authParams.put("grant_type", Config.getProperty("GRANT_TYPE"));
+		authParams.put("audience", Config.getProperty("AUDIENCE"));
+		authParams.put("clientSecret", Config.getProperty("CLIENT_SECRET"));
+		authParams.put("client_id", Config.getProperty("CLIENT_ID"));
+
 		requestSpecification.formParams(authParams);
-		
-		Response response = given().log().all()
-        .spec(requestSpecification)
-        .when()
-        .post()
-        .then()
-        .and()
-        .extract()
-        .response();
-		
-		System.out.println("Status received => " + response.getStatusLine()); 
+
+		Response response = given().log().all().spec(requestSpecification).when().post().then().and().extract()
+				.response();
+
+		System.out.println("Status received => " + response.getStatusLine());
 		System.out.println("Response=>" + response.prettyPrint());
 	}
 }
